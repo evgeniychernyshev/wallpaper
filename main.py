f5c4f31d1018d0595eab00a4c309e834584487fe
evgeniych <work.evgeniych@gmail.com>
2019-05-15 01:54:42 +0300

    Web interface added

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 from lib import calculate_rolls_count
+from flask import Flask, render_template, url_for, request
 
-roll_length = float(input('Enter roll length (m): '))
-roll_width = float(input('Enter roll width (m): '))
-room_length = float(input('Enter room length (m): '))
-room_width = float(input('Enter room width (m): '))
-ceiling_height = float(input('Enter ceiling height (m): '))
-rolls_count = calculate_rolls_count(roll_length, roll_width, room_length, room_width, ceiling_height)
-print('You need', rolls_count, 'rolls. We recommend buy 1-2 rolls more.')
+
+def main():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/calc', methods=['POST'])
+    def calc():
+        roll_length = float(request.form['roll_length'])
+        roll_width = float(request.form['roll_width'])
+        room_length = float(request.form['room_length'])
+        room_width = float(request.form['room_width'])
+        ceiling_height = float(request.form['ceiling_height'])
+        result = calculate_rolls_count(roll_length, roll_width, room_length, room_width, ceiling_height)
+        return render_template('index.html',
+                               rolls_count=result,
+                               roll_length=roll_length,
+                               roll_width=roll_width,
+                               room_length=room_length,
+                               room_width=room_width,
+                               ceiling_height=ceiling_height)
+
+    app.run(port=9878, debug=True)
+
+
+if __name__ == '__main__':
+    main()
