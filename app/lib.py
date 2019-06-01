f306e769cb08f653f81eac042dd731c612b8ba94
evgeniych <work.evgeniych@gmail.com>
2019-06-01 19:22:05 +0300

    Fixed #5

--- app/lib.py
+++ app/lib.py
@@ -1,4 +1,4 @@
-# считаем общее кол-во рулонов
+# calculating total count of wallpaper rolls
 def calculate_rolls_count(roll_l, roll_w, room_l, room_w, ceiling_h):
     room_perimeter = calculate_perimeter(room_l, room_w)
     canvas_count = calculate_canvas_count(room_perimeter, roll_w)
@@ -11,23 +11,22 @@ def calculate_rolls_count(roll_l, roll_w, room_l, room_w, ceiling_h):
     return int(rolls_count_total)
 
 
-# считаем периметр комнаты
 def calculate_perimeter(room_l, room_w):
     return (room_l + room_w) * 2
 
 
-# считаем общее кол-во полотнищ на комнату
+# calculating total count of wallpaper canvases for room
 def calculate_canvas_count(perimeter, roll_w):
     count = perimeter / roll_w
     rest = perimeter % roll_w
     if rest > 0:
-        return int(perimeter // roll_w + 1)  # округляем количество полотнищ в большую сторону
+        return int(perimeter // roll_w + 1)  # round canvases count
 
     return int(count)
 
 
-# считаем кол-во полотнищ в 1 рулоне
+# calculating count of canvases in 1 roll
 def calculate_canvas_in_roll(roll_l, ceiling_h):
-    fund = 0.1  # добавляем к высоте потолка 10см прозапас
+    fund = 0.1  # adding 10cm to ceiling height as reserve
     return int(roll_l // (ceiling_h + fund))
 
